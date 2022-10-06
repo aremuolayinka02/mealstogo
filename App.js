0f44928a1f15dbc7299fa37336a5e0901b7da6ad
Olayinka.Dev <aremuolayinka02@gmail.com>
2022-10-06 11:29:51 +0100

    Fixed the issue with Eslint and Prettier

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-
     <>
       <SafeAreaView style={{ flex: 1 }}>
-
         <View>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
       </SafeAreaView>
     </>
-
   );
 }
 
-const styles = StyleSheet.create({ });
+// const styles = StyleSheet.create({});
