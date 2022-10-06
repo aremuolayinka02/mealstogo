a41d86f7921edc13e5fd98062741d595bebbe4bc
Olayinka.Dev <aremuolayinka02@gmail.com>
2022-10-06 09:27:32 +0100

    Initial commit of MealsToGo

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+
+        <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      </SafeAreaView>
+    </>
+
+  );
+}
+
+const styles = StyleSheet.create({ });
